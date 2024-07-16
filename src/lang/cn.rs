25e562df6234bb6e0cb041052918e2ab522b4bec
XKongJian <156444233+XKongJian@users.noreply.github.com>
2024-07-16 19:09:39 +0800

    Update cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -145,7 +145,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Failed to make direct connection to remote desktop", "无法直接连接到远程桌面"),
         ("Set Password", "设置密码"),
         ("OS Password", "操作系统密码"),
-        ("install_tip", "QQ群704371359"),
+        ("install_tip", "QQ群:704371359"),
         ("Click to upgrade", "点击这里升级"),
         ("Click to download", "点击这里下载"),
         ("Click to update", "点击这里更新"),
